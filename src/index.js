1e6f2c6f06a3b677664db466ae232f2a8d8554df
Brian-15 <bcirieco@gmail.com>
2021-12-01 17:07:51 -0800

    stocked up fruits. added console dialogue for fruit transaction.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { choice, remove } from './helpers';
+import fruits from './foods';
+
+const fruit = choice(fruits);
+
+console.log(`I'd like one ${fruit}, please.`);
+console.log(`Here you go: ${fruit}`);
+console.log('Delicious! May I have another?');
+remove(fruits, fruit);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} left.`);
